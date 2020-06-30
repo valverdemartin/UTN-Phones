bd69daa197037a2fbcc077adc79374d1b6555995
Emi <eherrada@gmail.com>
2020-06-30 18:43:50 -0300

    Fix on Users.SQL

--- DB Scripts/Users.sql
+++ DB Scripts/Users.sql
@@ -9,7 +9,7 @@ grant select on utnphones.bills to 'client'@'localhost' IDENTIFIED BY 'Passw0rdC
 CREATE USER 'backoffice'@'localhost' IDENTIFIED BY 'Passw0rdBackOffice';
 grant insert, select, update, delete on utnphones.users to 'backoffice'@'localhost' IDENTIFIED BY 'Passw0rdBackOffice';
 grant insert, select, update, delete on utnphones.phone_lines to 'backoffice'@'localhost' IDENTIFIED BY 'Passw0rdBackOffice';
-grant select on utnphones.rates to 'backoffice'@'localhost' IDENTIFIED BY 'Passw0rdBackOffice';
+grant SELECT, insert on utnphones.rates to 'backoffice'@'localhost' IDENTIFIED BY 'Passw0rdBackOffice';
 grant select on utnphones.calls to 'backoffice'@'localhost' IDENTIFIED BY 'Passw0rdBackOffice';
 grant select on utnphones.bills to 'backoffice'@'localhost' IDENTIFIED BY 'Passw0rdBackOffice';
 
